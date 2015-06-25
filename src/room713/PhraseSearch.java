d63e2d8cfc2db086d3a73888b34d19072c2604e7
ZJUZT <3120101111@zju.edu.cn>
2015-06-25 14:11:00 +0800

    。。

---
+++ src/room713/PhraseSearch.java
@@ -0,0 +1,20 @@
+package room713;
+
+import java.util.ArrayList;
+
+/**
+ * Created by think on 2015/6/24.
+ */
+public class PhraseSearch {
+
+    ArrayList<String> Query;
+    PhraseSearch(ArrayList<String> q){
+        Query = q;
+    }
+
+    public ArrayList<Integer> getResult(){
+        for(String item :Query){
+
+        }
+    }
+}
